5091225ac701c0cd9fcf476d0904af862d27e2ea
dlewis23 <donovan@veeapps.com>
2022-05-24 11:35:33 -0400

    Update with Apple Silicon Support
    Built with latest Ti SDK and added Apple Silicon Support.

--- iphone/Classes/TiJbchartModuleAssets.m
+++ iphone/Classes/TiJbchartModuleAssets.m
@@ -1,34 +1,24 @@
 /**
- * Ti.JBChart (https://github.com/benbahrenburg/Ti.JBChart)
- *
- * Titanium code Copyright (c) 2009-2014 by Ben Bahrenburg. All Rights Reserved.
- * Licensed under the terms of the Apache 2.0 License
- * Please see the LICENSE included with this distribution for details.
- *
- * Ti.JBChart is a Titanium wrapper for JBChartView
- * for more information please visit https://github.com/Jawbone/JBChartView
+ * This is a generated file. Do not edit or your changes will be lost
  */
-
 #import "TiJbchartModuleAssets.h"
 
 extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation TiJbchartModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
-	//##TI_AUTOGEN_BEGIN asset
-	//Compiler generates code for asset here
-	return nil; // DEFAULT BEHAVIOR
-	//##TI_AUTOGEN_END asset
+  
+
+  return nil;
 }
 
-- (NSData*) resolveModuleAsset:(NSString*)path
+- (NSData *)resolveModuleAsset:(NSString *)path
 {
-	//##TI_AUTOGEN_BEGIN resolve_asset
-	//Compiler generates code for asset resolution here
-	return nil; // DEFAULT BEHAVIOR
-	//##TI_AUTOGEN_END resolve_asset
+  
+
+  return nil;
 }
 
 @end
